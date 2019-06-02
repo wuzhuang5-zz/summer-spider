ef9441230cb14cfeb972347d7edadad2cca17af6
wuzhuang <wuz0316@gmail.com>
2019-06-02 12:51:36 +0800

    'init'

---
+++ src/main/java/com/wz/conf/SummerException.java
@@ -0,0 +1,12 @@
+package com.wz.conf;
+
+public class SummerException extends RuntimeException {
+
+    public SummerException() {
+        super();
+    }
+
+    public SummerException(String message) {
+        super(message);
+    }
+}
